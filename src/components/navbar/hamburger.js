e41e34ea0c6c20ccd7671086398269e60989f7f0
halfrice <halfrice@pm.me>
2019-02-20 21:25:01 -0800

    Collapseable Navbar based on State.

--- src/components/navbar/hamburger.js
+++ src/components/navbar/hamburger.js
@@ -1,18 +1,17 @@
 import React from "react"
 import { css } from "@emotion/core"
-import { Link } from "gatsby"
 
-class Button extends React.Component {
+class Hamburger extends React.Component {
   render() {
     return (
       <div
         css={css`
           display: flex;
+          height: 2.75rem;
           overflow: hidden;
         `}
       >
-        <Link
-          to={this.props.link}
+        <div
           css={css`
             padding: 0.618rem 1rem;
             text-decoration: none;
@@ -21,17 +20,12 @@ class Button extends React.Component {
               color: #999;
             }
           `}
-          activeStyle={{
-            color: "#999",
-            background: "black",
-            borderBottom: "0.1rem solid #ff0075"
-          }}
         >
-          {this.props.text}
-        </Link>
+          |||
+        </div>
       </div>
     )
   }
 }
 
-export default Button
+export default Hamburger
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,19 +3,100 @@ import { css } from "@emotion/core"
 import Button from "./button"
 
 class Navbar extends React.Component {
+  state = {
+    width: window.innerWidth,
+    mobile: window.innerWidth <= 768,
+    navVisible: window.innerWidth <= 768 ? "false" : "true"
+  }
+
+  handleHamburgerClick = () => {
+    console.log("navVisible: ", this.state.navVisible)
+    this.setState({ navVisible: !this.state.navVisible })
+  }
+
+  handleResize = () => {
+    const nextWidth = window.innerWidth
+    this.setState({
+      width: nextWidth,
+      mobile: nextWidth <= 768
+    })
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize)
+  }
+
+  renderHamburger() {
+    return (
+      <div>
+        <div
+          onClick={this.handleHamburgerClick}
+          css={css`
+            display: ${this.state.mobile ? "flex" : "none"};
+            padding: 0.618rem 1rem;
+            text-decoration: none;
+            color: white;
+            &:hover {
+              color: #999;
+            }
+          `}
+        >
+          |||
+        </div>
+      </div>
+    )
+  }
+
+  renderLinks() {
+    if (this.state.navVisible && this.state.mobile) {
+      return (
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <Button text={`Home`} link={`/`} />
+          <Button text={`Projects`} link={`/projects/`} />
+          <Button text={`About`} link={`/about/`} />
+          <Button text={`Contact`} link={`/contact`} />
+        </div>
+      )
+    } else {
+      return (
+        <div
+          css={css`
+          display: flex;
+          flex-direction: row};
+        `}
+        >
+          <Button text={`Home`} link={`/`} />
+          <Button text={`Projects`} link={`/projects/`} />
+          <Button text={`About`} link={`/about/`} />
+          <Button text={`Contact`} link={`/contact`} />
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div
+        className="navbar"
         css={css`
           display: flex;
+          height: ${this.state.mobile ? "2.75rem" : "100%"};
+          flex-direction: ${this.state.mobile ? "column" : "row"};
           background: #333;
           text-decoration: none;
         `}
       >
-        <Button text={`Home`} link={`/`} />
-        <Button text={`Projects`} link={`/projects/`} />
-        <Button text={`About`} link={`/about/`} />
-        <Button text={`Contact`} link={`/contact`} />
+        {this.renderHamburger()}
+        {this.renderLinks()}
       </div>
     )
   }
